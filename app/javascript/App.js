c951c17a03b802cf0c02321975dd2a9b2423f13c
rica213 <25f5k252l@mozmail.com>
2023-06-20 00:37:51 +0300

    Set up route in the App component

--- app/javascript/App.js
+++ app/javascript/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Greeting from "./components/Greeting";
 function App() {
   return (
-    <div>
-      <Greeting />
-    </div>
+    <Routes>
+      <Route exact path="/" Component={Greeting} />
+    </Routes>
   );
 }
 
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,6 +3,7 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import App from "./App";
@@ -10,6 +11,8 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>
 );
